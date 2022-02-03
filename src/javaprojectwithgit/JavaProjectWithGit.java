5788b1ad07cc6f781802683192183b3c6d728128
redOne <redouane.bouchekir@gmail.com>
2022-02-03 10:43:03 +0100

    3rd commit

--- src/javaprojectwithgit/JavaProjectWithGit.java
+++ src/javaprojectwithgit/JavaProjectWithGit.java
@@ -14,7 +14,7 @@ public class JavaProjectWithGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.err.println("First Changes");
         
     }
     
